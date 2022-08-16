030adf537d6fc661acef47d994840e4da34c05f1
Dreamsoldier <kocakali5834@gmail.com>
2022-08-16 12:25:57 +0300

    main.py rename

---
+++ main.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+mpHands = mediapipe.solutions.hands
+
+hands = mpHands.Hands()
+
+mpDraw = mediapipe.solutions.drawing_utils
+
+checkThumbsUp = False
+
+
+while 1:
+    ret, frame = cap.read()
+
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    hlms = hands.process(frameRGB)
+
+    height, width, channel = frame.shape
+
+    if hlms.multi_hand_landmarks:
+        for handlandmarks in hlms.multi_hand_landmarks:
+
+            for fingerNum, landmark in enumerate(handlandmarks.landmark):
+                positionX, positionY = int(landmark.x * width), int(landmark.y * height)
+
+                if fingerNum > 4 and landmark.y < handlandmarks.landmark[2].y:
+                    break
+
+                if fingerNum == 20 and landmark.y > handlandmarks.landmark[2].y:
+                    checkThumbsUp = True
+
+
+            mpDraw.draw_landmarks(frame, handlandmarks, mpHands.HAND_CONNECTIONS)
+
+
+
+    if checkThumbsUp:
+        from ctypes import cast, POINTER
+        from comtypes import CLSCTX_ALL
+        from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(
+            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        volume.SetMute(1, None)
+        break
+
+    frame = cv2.flip(frame, 1)
+    cv2.imshow("Webcam", frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
